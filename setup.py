6af48828366f7fbfcb95d4cf876bea43393aa1d2
Jodie Belgrave <rmb1993@gmail.com>
2015-09-27 14:57:46 -0400

    fix development build versions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hq-manager',
-    version='2.0.0-dev',
+    version='2.0.0.dev1',
     url='https://github.com/herqles-io/hq-manager',
     include_package_data=True,
     license='MIT',
@@ -11,15 +11,12 @@ setup(
     package_dir={'': 'src'},
     packages=find_packages('src'),
     install_requires=[
-        'hq-lib==1.0.0',
+        'hq-lib==2.0.0.dev1',
         'pika',
         'cherrypy==3.8.0',
         'pyyaml==3.11',
         'Routes==2.2',
         'schematics==1.1.0'
-    ],
-    dependency_links=[
-        'git+https://github.com/herqles-io/hq-lib.git@2.0-dev#egg=hq-lib-2.0.0-dev'
     ],
     scripts=['bin/hq-manager']
 )
